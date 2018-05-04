cbe0e7c459b575beaeb7557dd98beb54ead9e164
Hannah Mitchell <hannah@Hannahs-MacBook-Pro-2.local>
2018-05-04 13:09:40 -0700

    Checkers pause on mouse press

--- Checkers.java
+++ Checkers.java
@@ -7,7 +7,7 @@ import java.awt.event.*;
 import javax.swing.JButton;
 import javax.swing.JPanel;
 
-public class Checkers extends Applet implements Runnable
+public class Checkers extends Applet implements Runnable, MouseListener
 {
    private int redX, redY;  
    private int blackX, blackY;
@@ -45,6 +45,9 @@ public class Checkers extends Applet implements Runnable
       panel.add(goButton);
       panel.add(stopButton);
 
+      //for checkers to pause when applet is clicked
+      addMouseListener(this);
+
       //add panel to applet
       setLayout(new BorderLayout());
       add("South", panel);
@@ -54,7 +57,7 @@ public class Checkers extends Applet implements Runnable
       Thread t = new Thread (this);
       t.start();  // spawn the thread
    }
-   
+
    public void run()
    {
       int dy = 150;
@@ -64,7 +67,7 @@ public class Checkers extends Applet implements Runnable
       {
             for (int i = 0; i < 3; i++)
             {               
-               //sleep if stop button pressed or click held down
+               //sleep if stop button pressed or mouse pressed
                while (running == false)
                {
                   try
@@ -78,6 +81,7 @@ public class Checkers extends Applet implements Runnable
                blackY += (dy / -1);
                repaint();
                
+               //pause before next movement
                try
                {
                   Thread.sleep(500);
@@ -107,6 +111,23 @@ public class Checkers extends Applet implements Runnable
       g.fillOval(blackX, blackY, 100, 100);
    }
 
+   //click events
+   public void mousePressed(MouseEvent e) 
+   {
+      running = false;
+   }
+
+   public void mouseReleased(MouseEvent e) 
+   {
+      running = true;
+   }
+
+   //no actions for following overloaded MouseListener methods
+   public void mouseEntered(MouseEvent e) { } //do nothing
+   public void mouseExited(MouseEvent e) { }  //do nothing
+   public void mouseClicked(MouseEvent e) { } //do nothing
+
+   //button events
    private class GoButtonHandler implements ActionListener
    {
       public void actionPerformed(ActionEvent e)
